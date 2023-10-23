5f5e70623203cca35e4ab8625bd43778cd40c08c
sudhanshu7sb <sudhanshu7.sb@gmail.com>
2023-10-23 15:47:41 +0530

    add project

---
+++ index.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+
+
+const app = express();
+app.use(express.json());
+
+
+// middleware
+
+app.use((req,res,next) => {
+    console.log(req.path,req.method);
+    next();
+})
+
+// route 
+
+app.use("/api/user",userRoutes);
+
+
+
+
+// connect to db
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("connected to db")
+}).catch(error => {
+    console.log(error)
+})
+
+// listen app on this port
+app.listen(process.env.PORT,()=>{
+    console.log("server started on port : 5000")
+})
